ce3b4a915264863ec05d80ebd8254309db9070d4
John (Ottenlips) Franke <jottenlips@gmail.com>
2022-12-16 20:23:06 -0600

    Update iamrprompt.sh

--- iamrprompt.sh
+++ iamrprompt.sh
@@ -1,10 +1,9 @@
-alias whoiam="aws sts get-caller-identity"
-function getArn () {
-    node -pe 'JSON.parse(process.argv[1])["Arn"]' "$(whoiam)"
+function whoiam () {
+    node -pe 'JSON.parse(process.argv[1])["Arn"]' "$(aws sts get-caller-identity)"
 }
 function refreshIamPrompt() {
     echo 'You are interacting with AWS as'
-    getArn
-    RPROMPT=$(getArn)
+    whoiam
+    RPROMPT=$(whoiam)
 }
 refreshIamPrompt
